0e29e35d3e48b851c5ff6b0362248f14f092ac6b
siddhartha <vsiddharth010@gmail.com>
2025-10-21 00:56:40 +0530

    data trasnformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,7 +5,8 @@ from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -42,4 +43,7 @@ class DataIngestion:
             raise CustomException(e, sys)   
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.intiate_data_ingestion()
+    train_data,test_data=obj.intiate_data_ingestion()
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+    
